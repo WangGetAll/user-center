8b497f78e5adf492997da5d83a1e000c111b99d2
wangjiayu <wangdequan1126@gmail.com>
2024-06-25 16:38:18 +0800

    bugfix: 解决swagger不兼容

--- src/main/java/com/wjy/usercenter/UserCenterApplication.java
+++ src/main/java/com/wjy/usercenter/UserCenterApplication.java
@@ -3,8 +3,10 @@ package com.wjy.usercenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
 public class UserCenterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/wjy/usercenter/config/swagger/SwaggerConfig.java
+++ src/main/java/com/wjy/usercenter/config/swagger/SwaggerConfig.java
@@ -1,83 +1,38 @@
 package com.wjy.usercenter.config.swagger;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+    @Value("${spring.swagger.enable}")
+    private boolean enable;
 
-    @Value("${spring.swagger2.enabled}")
-    private Boolean enabled;
-
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .enable(enabled)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wjy.usercenter"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    public ApiInfo apiInfo() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("api")
-                .description("author:wjy")
+                .title("user-center服务api文档")
                 .version("1.0")
                 .build();
     }
 
     @Bean
-    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-        return new BeanPostProcessor() {
-
-            @Override
-            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
-                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-                }
-                return bean;
-            }
-
-            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
-                List<T> copy = mappings.stream()
-                        .filter(mapping -> mapping.getPatternParser() == null)
-                        .collect(Collectors.toList());
-                mappings.clear();
-                mappings.addAll(copy);
-            }
-
-            @SuppressWarnings("unchecked")
-            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-                try {
-                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-                    field.setAccessible(true);
-                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    throw new IllegalStateException(e);
-                }
-            }
-        };
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .enable(enable)
+                .select()
+                //扫描basePackage包下面的“/rest/”路径下的内容作为接口文档构建的目标
+                .apis(RequestHandlerSelectors.basePackage("com.wjy.usercenter"))
+                //.paths(PathSelectors.regex("/rest/.*"))
+                .build();
     }
+
 
 }
